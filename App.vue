b776ce3f93190bb94ea88788a75e488d40c0bc26
Виктор Яковлев <0rst1yt@gmail.com>
2023-10-09 17:11:16 +0300

    App is commited

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cube">
+    
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+
+  .cube {
+    width: 40px;
+    height: 40px;
+    background-color: red;
+  }
+
+</style>
